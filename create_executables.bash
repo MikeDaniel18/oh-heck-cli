66a4cb895db2473363c92cbd0e539563f81f7b10
Michael Daniel <mdaniel@handymaninteractive.com>
2022-03-15 11:17:53 +0000

    Zipped executables
    - Executables are now zipped

--- create_executables.bash
+++ create_executables.bash
@@ -7,7 +7,8 @@ do
 	platform_split=(${platform//\// })
 	GOOS=${platform_split[0]}
 	GOARCH=${platform_split[1]}
-	output_name='dist/oh-heck-'$GOOS'-'$GOARCH'/oh-heck'
+	directory_name="dist/oh-heck-$GOOS-$GOARCH"
+	output_name="$directory_name/oh-heck"
 	# if [ $GOOS = "windows" ]; then
 	# 	output_name+='oh-heck.exe'
 	# fi	
@@ -18,6 +19,12 @@ do
 		exit 1
 	else
 		chmod +x "$output_name"
+		
+		if [ "$GOOS" = "linux" ]; then
+			(cd $directory_name && tar -xzvf oh-heck.tar.gz oh-heck)
+		elif [ "$GOOS" = "darwin" ]; then
+			(cd $directory_name && zip -r oh-heck.zip oh-heck)
+		fi
 	fi
 done
 
